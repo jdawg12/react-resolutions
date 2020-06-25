f105b751dce4cd590555d1f915b9a09d69927200
Joshua Wang <37763916+jdawg12@users.noreply.github.com>
2020-06-25 15:04:54 -0400

    Testing out React components

---
+++ react-resolutions-core/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+
+  render () {
+    return(<div> App component test</div>)
+  }
+}
+
+export default App;
--- test.js
+++ test.js
@@ -1,3 +1,7 @@
 var myString = "What is react and why is it useful."
 myString.length;
 myString.toUpperCase;
+var notEmptyObject = {
+    'label': 'value',
+    'label2': 'value2',
+}
