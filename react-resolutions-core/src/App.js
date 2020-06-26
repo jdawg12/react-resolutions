8f0c33b50100015fa34203ca1b4afd8189499e9a
Joshua Wang <37763916+jdawg12@users.noreply.github.com>
2020-06-26 12:22:54 -0400

    Creating states to update todoItems and todoList

--- react-resolutions-core/src/App.js
+++ react-resolutions-core/src/App.js
@@ -7,14 +7,31 @@ import TodoItem from './TodoItem/todoItem';
 
 class App extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      todos : [] /*State is an object that you're creating. We assign it the key todos with an empty array as value
+                 Whenever state is updated, it will re-render everything in the render function with the updated state*/ 
+    }
+  }
+
   render () {
     return(
       <div>
-          <TodoItem></TodoItem>
+          <TodoItem></TodoItem> {/*can only have one div within this render, but it can have multiple sub-divs*/}
           <TodoList></TodoList>
       </div>
       );
   }
+
+  componentDidMount = () => {
+    const todos = localStorage.getItem('todos');
+    if(todos){
+      console.log('Has todos', todos) //Can write strings and objects to the console
+    } else {
+        console.log('No todos', todos);
+    }
+  } 
 }
 
 export default App;
