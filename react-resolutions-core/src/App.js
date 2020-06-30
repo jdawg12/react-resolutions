e68bfc3283d9e708a4f82efdd251ae8e578d1d4a
Joshua Wang <37763916+jdawg12@users.noreply.github.com>
2020-06-30 13:32:54 -0400

    more notes on React specific methods and updates to TodoList class

--- react-resolutions-core/src/App.js
+++ react-resolutions-core/src/App.js
@@ -22,6 +22,7 @@ class App extends React.Component {
       /*We can use JS within HTML here to pass on the reference of state. This is partly what makes React so pwoerful*/
       <div>
         <AddToDO addToDoFn={this.addToDo}></AddToDO>
+        <TodoList todos={this.state.todos}></TodoList>
       </div>
       );
   }
@@ -39,7 +40,8 @@ class App extends React.Component {
   
   addToDo = async (todo) => { //tells browser that this function is asynchronous, so we can use the 'await' function within
     await this.setState({todos : [...this.state.todos, todo]}); //the ... is the Spread Operator. Take everything in this.state.todos, combine with whatever is in todo, and save it to this.todos. This spread operator is also ASYNCHRONOUS so
-    
+    localStorage.setItem('todos', JSON.stringify(this.state.todos)) //saves the value of this.state.todos to a key called 'todos'
+    console.log(localStorage.getItem('todos'));
   } 
 }
 
--- react-resolutions-core/src/TodoList/todoList.js
+++ react-resolutions-core/src/TodoList/todoList.js
@@ -3,7 +3,16 @@ import React from 'react';
 class TodoList extends React.Component{
 
     render(){
-        return(<div>LIST LIST LIST</div>);
+
+        const { todos } = this.props; //same as writing const todos = this.props.todos; Allows you to pull out MULTIPLE props at the same time. Destructuring. 
+
+        return( //here we are again, creating a parent container that will contain the javascript involved to iterate through the items in todos
+            <div className='todoListContainer'>
+                {
+
+                }
+            </div>
+        );
     }
 }
 
